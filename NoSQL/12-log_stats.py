2ed11ec69875737f7a8aa03e3ec731051696213e
Fahad-9195 <fahad-9195@hotmail.com>
2026-03-06 22:22:05 +0300

    Update MongoDB connection string and print format

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,22 +3,19 @@
 
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
-    client = MongoClient()
+    client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.logs
     collection = db.nginx
 
-    print(f"{collection.count()} logs")
+    print("{} logs".format(collection.count()))
     print("Methods:")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     for method in methods:
-        print("\tmethod {}: {}".format(
-            method,
-            collection.count({"method": method})
-        ))
+        print("\tmethod {}: {}".format(method,
+              collection.count({"method": method})))
 
     print("{} status check".format(
         collection.count({"method": "GET", "path": "/status"})
